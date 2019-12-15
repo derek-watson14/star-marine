786f84ae6582b954e6cd597dfdee90486404abe5
Derek Watson <derek.watson92@gmail.com>
2019-12-15 14:39:44 +0100

    greatly simplified scroll checking

--- static/scripts/tugs.js
+++ static/scripts/tugs.js
@@ -1,49 +1,15 @@
-class Cards {
-    constructor(className = 'boat-card') {
-        const rawList = document.querySelectorAll(`.${className}`);
-        const cardInfo = [];
-        rawList.forEach(function(card) {
-            cardInfo.push({
-                name: card.id,
-                classes: card.classList,
-                bottom: Math.round(window.scrollY + card.getBoundingClientRect().bottom)
-            });
-        });
-        this.cards = cardInfo;
-    }
-};
+const boatCards = document.querySelectorAll('.boat-card');
 
-let cards = new Cards();
+function focusCard(target) {
+    boatCards.forEach(card => {
+        if (card.id === target.id) card.classList.add('bc--inView');
+        else card.classList.remove('bc--inView');
+    })
+}
 
-window.addEventListener('resize', () => { cards = new Cards(); }),
-
-window.addEventListener("scroll", function changeCards() {
-    function focusCurrent(focusedBoat) {
-        cards.cards.forEach(boat => {
-            if (boat.name === focusedBoat) boat.classes.toggle('bc--inView', true);
-            else boat.classes.toggle('bc--inView', false);
-        });
-    };
-
-    function atBottom() {
-        const windowBottom = Math.round(document.body.offsetHeight);
-        const scrollBottom = Math.round(window.innerHeight + window.scrollY);
-        return windowBottom - scrollBottom < 1 ? true : false;
-    };
-
-    function determineFocus() {
-        const windowCenter = Math.round(window.scrollY + (window.innerHeight / 2));
-        
-        for (let c of cards.cards) {
-            if (c.bottom >= windowCenter) {
-                focusCurrent(c.name);
-                break;
-            }
-        }
-    }
-
-    (function testPosition() {
-        if (atBottom()) focusCurrent(cards.cards[cards.cards.length - 1].name);
-        else determineFocus();
-    })();
-})
+boatCards.forEach(card => {
+    const ob = new IntersectionObserver(payload => {
+        if (payload[0].isIntersecting) focusCard(card);
+    }, { threshold: 0.75 })
+    ob.observe(card);
+});
